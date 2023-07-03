83398c57591960761ad8e73269695d65a0711617
sadrul <sadrul@neocon.com>
2023-07-03 18:27:49 +0600

    TECH-13-ngw-back-end-contact

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\api\ContactController;
 use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -23,3 +24,9 @@ use Illuminate\Support\Facades\Route;
 Route::post('/registration', [AuthController::class, 'registration']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout']);
+//contact
+Route::prefix('contact')->group(function () {
+    Route::get('/create', [ContactController::class, 'create'])->name('contact.create');
+    Route::get('/index', [ContactController::class, 'index'])->name('contact.index');
+    Route::post('/store', [ContactController::class, 'store'])->name('contact.store');
+});
