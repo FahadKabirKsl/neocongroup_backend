79bd145d44a7ce4e9e19ce151f55b2829babb95e
sadrul <sadrul@neocon.com>
2023-07-04 01:08:46 +0600

    TECH-20-ngw-back-end-employee-api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\api\EmployeeController;
 use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -23,3 +24,11 @@ use Illuminate\Support\Facades\Route;
 Route::post('/registration', [AuthController::class, 'registration']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout']);
+
+//employee
+Route::prefix('employee')->group(function () {
+    Route::get('/', [EmployeeController::class, 'index']);
+    Route::post('/store', [EmployeeController::class, 'store']);
+    Route::post('/{id}', [EmployeeController::class, 'update']);
+    Route::delete('/delete/{id}', [EmployeeController::class, 'destroy']);
+});
